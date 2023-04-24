a097de9454420affbab57c2acd2c66b7819a2dda
Averill Cate, Jr <acate@arizona.edu>
2023-04-24 14:47:06 -0700

    Clean out for elixir branch

--- Dockerfile.pgsql
+++
@@ -1,3 +0,0 @@
-FROM mdillon/postgis:latest
-RUN apt-get update -y --fix-missing
-EXPOSE 5432
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from graphene_django.views import GraphQLView
-
-app_name = 'api'
-
-urlpatterns = [
-    path('graphql', GraphQLView.as_view(graphiql=True)),
-]
