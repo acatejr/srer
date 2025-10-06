73f1a8b5dbc52660aca1e3060b1470a125b7ce09
acatejr@gmail.com <acatejr@gmail.com>
2025-10-06 16:39:07 -0700

    Refactoring project

--- precip/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrecipConfig(AppConfig):
-    name = 'precip'
--- scripts/run_dev_server.py
+++
@@ -1,4 +0,0 @@
-
-def run():
-    port = 8000
-    print("Dev server listening on port: {}".format(port))
